939cc7ebde563893c43d30f333e2400b0e9c6478
Cramelsim <159296865+Cramelsim@users.noreply.github.com>
2024-11-18 21:19:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ function introduction(name) {
   function introductionWithLanguageOptional(name, language="JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-  
